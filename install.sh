5fe7d172bd1c7df40ce39ca261c10e4cd0953255
Isaac Spencer <isaacspencer17@gmail.com>
2020-12-06 18:01:53 -0700

    updates/fixes on install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# Pre-run checks
+if [ $(id -u) -ne 0 ]; then
+    echo "This script must be run with root permissions."
+    exit
+fi
+
 # Setup
-echo -n Starting installation, executing setup commands...
+echo -n "Starting installation, executing setup commands ... "
 cd "$(dirname "$0")"
 cp -r pihub-client /opt/
 cp README.md /opt/pihub-client/README.md
-echo finished, beginning verification process.
+echo finished
+echo Beginning verification process.
 
 # Verify device
+domain="localhost:8000"
 echo -n "Enter your device public key: "; read pubkey
-
-idresp = $(curl -s -w '%{http_code}' -X POST -d public_key=$pubkey http://pihub.site/api/identify-device)
+idresp=$(curl -s -w '%{http_code}' -X POST -d public_key=$pubkey http://$domain/api/identify-device)
 
 if [ "$idresp" != "200" ]; then
     echo Error: Could not find device with the provided public key.
@@ -19,9 +26,9 @@ fi
 
 echo -n "Enter your device private key: "; read privkey
 currtime = $(date +%s)
-vhash = $(echo -n "$pubkey$currtime$privkey" | sha256sum)
-vdata = "public_key=$pubkey&time=$currtime&vhash=$vhash"
-vresp = $(curl -w '%{http_code}' -X POST -d $vdata http://pihub.site/api/verify-device)
+vhash=$(echo -n "$pubkey$currtime$privkey" | sha256sum)
+vdata="public_key=$pubkey&time=$currtime&vhash=$vhash"
+vresp=$(curl -w '%{http_code}' -X POST -d $vdata http://$domain/api/verify-device)
 
 if [ "$vresp" != "200" ]; then
     echo Error: Invalid private key.
@@ -32,7 +39,7 @@ else
 fi
 
 # Set up services
-echo Setting up systemd services...
+echo -n Setting up systemd services...
 cp resources/* /etc/systemd/system/
 systemctl start pihub.service
 systemctl enable pihub.service
--- pihub-client/scripts/update_pihub.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-ip = requests.get("https://api.ipify.org").content.decode("utf-8")
-requests.post("http://pihub.site/api/device-update", {"ip": ip})
